04957820466028aca9f9f23828d1de3bf03afb3e
DevFabJava <dev.fabioalmeida@proton.me>
2025-06-30 14:36:55 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::vec::Vec;
+#[derive(Debug)]
+
+
+struct Deck {
+    cards: Vec<String>,
+}
+
+
+fn main() {
+    let deck: Deck = Deck { cards: Vec::new() };
+
+    println!("Heres your deck: {:?}", deck);
+}
