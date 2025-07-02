112f0a04bf0f5d65078638a23c590b559278b567
DevFabJava <dev.fabioalmeida@proton.me>
2025-07-01 23:35:13 -0300

    feat: Implement basic Deck struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::vec::Vec;
+use std::{collections::btree_map::Values, vec::Vec};
 #[derive(Debug)]
 
 
@@ -8,7 +8,31 @@ struct Deck {
 
 
 fn main() {
-    let deck: Deck = Deck { cards: Vec::new() };
+    //Listof 'suits' - 'hearts', 'spades'
+    //List of 'values' - 'ace', 'two', 'three'
+
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three","Four", "Five",
+    "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+    //Empty vector(mutable)
+
+    let mut  cards = vec![] ;
+    
+
+    //Double nested for loop
+
+    for suit in suits  {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+        
+    }
+
+
+
+    let deck: Deck = Deck { cards };
 
     println!("Heres your deck: {:?}", deck);
 }
